7aaae45b3ce96619814f0c5dc7406d5404e9deab
Sefa Altun <113822915+sefaaltun06@users.noreply.github.com>
2024-02-14 14:17:17 +0300

    Add files via upload
    Konsoldan girilen bir değerin 5 sayısından büyük olup olmadığını kontrol eden ve bu olayı döngüye sokan bir program.

---
+++ sayikontrol edici.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SayiKontrolEdici
+{
+    class SayiKarsilastirici
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Bir değer giriniz: ");
+                var x = Convert.ToInt32(Console.ReadLine());
+            
+                if (x > 5)
+                {
+                    Console.WriteLine("Girdiğiniz değer 5'ten büyük.");
+                }
+            
+                else if (x < 5)
+                {
+                    Console.WriteLine("Girdiğiniz değer 5'ten küçük. ");
+                }
+            
+                else
+                {
+                    Console.WriteLine("Girdiğiniz değer 5'e eşit.");
+                }
+            }
+            
+        }      
+    }
+}    
